6cd74d7488ba1f38a0e8d4d66723a5ceb6f3dfca
Ajaybalajiprasad <ajaybalajiprasad02@gmail.com>
2026-01-06 13:29:17 +0530

    fix: leetcode api ranking

--- pkg/fetcher/leetcode.go
+++ pkg/fetcher/leetcode.go
@@ -4,27 +4,31 @@ import (
 	"bytes"
 	"encoding/json"
 	"net/http"
+	"strconv"
 )
 
 type lcRequest struct {
-	Query     string                 
-	Variables map[string]interface{} 
+	Query     string `json:"query"`
+	Variables map[string]interface{} `json:"variables"`
 }
 
 type lcResponse struct {
-	Data struct {
-		MatchedUser struct {
-			Profile struct {
-				Ranking int
-			}
-			SubmitStats struct {
-				AcSubmissionNum []struct {
-					Count int
-					Difficulty string
-				}
-			}
-		}
-	}
+    Data struct {
+        MatchedUser struct {
+            Profile struct {
+                Ranking int `json:"ranking"`
+            } `json:"profile"`
+            SubmitStats struct {
+                AcSubmissionNum []struct {
+                    Count      int    `json:"count"`
+                    Difficulty string `json:"difficulty"`
+                } `json:"acSubmissionNum"`
+            } `json:"submitStats"`
+        } `json:"matchedUser"`
+		UserContestRanking struct {
+			Rating float64 `json:"rating"` 
+		} `json:"userContestRanking"`
+    } `json:"data"`
 }
 
 func fetchLeetCode(username string) (Profile, error) {
@@ -41,20 +45,28 @@ func fetchLeetCode(username string) (Profile, error) {
 				}
 			}
 		}
+		userContestRanking(username: $username) {
+            rating
+        }
 	}`
 
 	reqBody := lcRequest{
 		Query:     query,
 		Variables: map[string]interface{}{"username": username},
 	}
-
 	payload, _ := json.Marshal(reqBody)
 
-	resp, err := http.Post("https://leetcode.com/graphql", "application/json", bytes.NewBuffer(payload))
-	if err != nil {
-		return Profile{}, err
+	req, _ := http.NewRequest("POST", "https://leetcode.com/graphql", bytes.NewBuffer(payload))
+
+	req.Header.Set("Content-Type", "application/json")
+    req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+    if err != nil {
+        return Profile{}, err
 	}
-	defer resp.Body.Close()
+    defer resp.Body.Close()
 
 	var result lcResponse
 	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
@@ -70,7 +82,8 @@ func fetchLeetCode(username string) (Profile, error) {
 		UserName:  username,
 		Platform:  "leetcode",
 		Name:      username,
-		Rating:    result.Data.MatchedUser.Profile.Ranking,
+		Rating:    int(result.Data.UserContestRanking.Rating),
+		Rank:      strconv.Itoa(result.Data.MatchedUser.Profile.Ranking),
 		SolvedCount: totalSolved,
 	}, nil
 }
